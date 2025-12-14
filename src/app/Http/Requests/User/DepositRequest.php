c9744d5a9ac4d40560823a1c4e47238293eeeb64
Felipe <felipeampolini@hotmail.com>
2025-12-14 14:12:26 -0300

    refactor: remove overwrite desnecessario em DepositRequest

--- src/app/Http/Requests/User/DepositRequest.php
+++ src/app/Http/Requests/User/DepositRequest.php
@@ -6,10 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class DepositRequest extends FormRequest
 {
-    public function authorize(): bool
-    {
-        return true;
-    }
 
     public function rules(): array
     {
